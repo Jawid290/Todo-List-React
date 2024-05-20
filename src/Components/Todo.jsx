2e1fa2d5f8e288c79b96cb32df10c289e8d10acb
Jawid <Jawid.hashemi290@gmail.com>
2024-05-20 17:14:48 +0430

    Adding Buttons

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,18 +1,40 @@
 
 import "./CSS/Todo.css"
+import { useState  } from "react";
+import {useRef} from "react";
+import {useEffect} from "react";
+let count  = 0; 
 const Todo = () => {
+
+  const [todos, setTodos]= useState([]);
+  const inputRef = useRef(null); 
+
+  const add = ()=>{
+   
+   setTodos([...todos,{no: count++,text:inputRef.current.value, display:""}]);
+    inputRef.current.value = "";
+  }
+
+  useEffect(()=>{
+    console.log(todos);
+  }, [todos])
+    
   return (
     <div>
 
         <div className="todo">
            <div className="todo-header">To-DO List</div> 
            <div className="todo-add">
-                <input type="text" placeholder="Add Your Task" className="todo-input"/>
-                <div className="todo-add-btn">ADD</div>
+                <input ref={inputRef} type="text" placeholder="Add Your Task" className="todo-input"/>
+                <div onClick={()=>{add()}} className="todo-add-btn">ADD</div>
            </div>
            
            <div className="todo list"></div>
-           
+
+              {todos.map((item,index)=>{
+                return <TodoItems key={index} no={item.no} display= {item.display} text={item.text} />
+              })}
+
         </div>
 
       
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,10 +1,27 @@
 
 import "./CSS/TodoItems.css"
-const TodoItems = () => {
+
+import tick from "./Assets/tick.png";
+import not from "./Assets/not.png";
+import cross from "./Assets/cross.png";
+
+
+
+
+
+
+const TodoItems = ({no,dispaly,text}) => {
   return (
-    <div>
-      
-    </div>
+    <div className= "todoitems" >
+      <div calssName="todoitems-container">
+      <img src={not} alt="" />
+      <img src={tick} alt=""/>
+      <div className="todoitems-text">{text}</div>
+
+      </div>
+      <img src={cross} alt=""/>
+
+    </ div>
   )
 }
 
